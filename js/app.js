f2cfa32f2695e402af5705751611a8037c8b55e0
Mahmudul-H-Noman <mhnoman75@gmail.com>
2023-02-18 20:07:19 +0600

    update blog page

--- js/app.js
+++ js/app.js
@@ -1,17 +1,15 @@
-// card 1 : triangle
 let serial = 0;
+
+// card 1 : triangle
 document.getElementById('triangle-btn').addEventListener('click', function(){
     const triangle_b_Value = getInputFieldValueById('triangle-b-field');
     const triangle_h_Value = getInputFieldValueById('triangle-h-field');
     const triangleFixedValue = parseFloat(0.5);
-
-
     const triangleTotalResult = triangleFixedValue * triangle_b_Value * triangle_h_Value;
     const card_Name = document.getElementById("triangle-title").innerText;
     serial += 1;
     displayData(card_Name, triangleTotalResult);
 })
-
 
 // card 2 : Rectangle
 document.getElementById('rectangle-btn').addEventListener('click', function(){
@@ -23,7 +21,6 @@ document.getElementById('rectangle-btn').addEventListener('click', function(){
     displayData(card_Name, rectangleTotalResult);
 })
 
-
 // card 3 : Parallelogram
 document.getElementById('parallelogram-btn').addEventListener('click', function(){
     const parallelogramResult = getInnerElementValueById('parallelogram-b-value', 'parallelogram-h-value');
@@ -31,7 +28,6 @@ document.getElementById('parallelogram-btn').addEventListener('click', function(
     serial +=1;
     displayData(card_Name, parallelogramResult);
 })
-
 
 // card 4 : Rhombus
 document.getElementById('rhombus-btn').addEventListener('click', function(){
@@ -43,7 +39,6 @@ document.getElementById('rhombus-btn').addEventListener('click', function(){
     displayData(card_Name, rhombusFinalResult);
 })
 
-
 // card 5 : Pentagon
 document.getElementById('pentagon-btn').addEventListener('click', function(){
     const pentagonFixedValue = 0.5;
@@ -53,7 +48,6 @@ document.getElementById('pentagon-btn').addEventListener('click', function(){
     serial +=1;
     displayData(card_Name, pentagonFinalResult);
 })
-
 
 // card 6 : Ellipse
 document.getElementById('ellipse-btn').addEventListener('click', function(){
@@ -66,7 +60,7 @@ document.getElementById('ellipse-btn').addEventListener('click', function(){
 })
 
 
-
+// CARD BACKGROUND COLOR CHANGE RANDOMLY :::::::::::::::::::::::::::::::::::::::
 
 // card 1 random background color
 document.getElementById('triangle-random-color').addEventListener('mouseover', function(){
